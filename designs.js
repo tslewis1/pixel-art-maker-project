8653aa2b28bf53c77c8e58aa7c2f7990558fd450
Taryn Lewis <tarynslewis@gmail.com>
2017-11-02 13:07:51 -0700

    Move width and height variables above makeGrid function

--- designs.js
+++ designs.js
@@ -1,15 +1,16 @@
 // Select color input
 // Select size input
 
+// Define variables for width and height of grid
+
+let width = $("#input_width").val();
+
+let height = $("#input_height").val();
+
+
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Define variables for width and height of grid
-
-	let width = $("#input_width").val();
-
-	let height = $("#input_height").val();
 
 // This for loop creates each table row and cell that the user inputs as width and height
 

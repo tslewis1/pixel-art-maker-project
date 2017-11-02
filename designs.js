2f37bbda9b3803b97d73423d3f00b4074f8532ed
Taryn Lewis <tarynslewis@gmail.com>
2017-11-02 12:59:04 -0700

    Create for loop that makes grid

--- designs.js
+++ designs.js
@@ -5,6 +5,13 @@
 
 function makeGrid() {
 
-// Your code goes here!
+// This for loop creates each table row and cell that the user inputs as width and height
+
+for (i=1; i<=width; i++) {
+	$("#pixel_canvas").append("<tr></tr>");
+	for (j=1; j<=height; j++) {
+		$("#pixel_canvas").children("tr").last().append("<td></td>");
+	}
+}
 
 }

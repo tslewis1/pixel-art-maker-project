208f0575fe2c5e08b069edb7ffda09e93d9f80b5
Taryn Lewis <tarynslewis@gmail.com>
2017-11-02 13:04:48 -0700

    Add variables for width and height of the grid
    Also indents everything within the makeGrid function

--- designs.js
+++ designs.js
@@ -5,13 +5,19 @@
 
 function makeGrid() {
 
+// Define variables for width and height of grid
+
+	let width = $("#input_width").val();
+
+	let height = $("#input_height").val();
+
 // This for loop creates each table row and cell that the user inputs as width and height
 
-for (i=1; i<=width; i++) {
-	$("#pixel_canvas").append("<tr></tr>");
-	for (j=1; j<=height; j++) {
-		$("#pixel_canvas").children("tr").last().append("<td></td>");
+	for (i=1; i<=width; i++) {
+		$("#pixel_canvas").append("<tr></tr>");
+		for (j=1; j<=height; j++) {
+			$("#pixel_canvas").children("tr").last().append("<td></td>");
+		}
 	}
-}
 
 }
